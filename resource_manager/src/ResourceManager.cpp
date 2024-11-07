SGCMarkus/android_vendor_qcom_opensource_pal

pal: ringtone + music and SREC/VBC with voice call concurrency on BLE
-In combo use case, if ringtone routed to a2dp + spkr and at that time
 a2dp/ble device is in suspended state, so during resume ringtone won't
 be able to route to BLE device. In that case, add both speaker and
 a2dp/ble into suspended devices list so that a2dp/ble will be restored
 during a2dpResume without removing speaker from ringer stream

-During ongoing BLE call, if stereo record/VBC stream opens on BLE
 causes voice CIS disconection because of incoming and current dev
 params are different. It is avoided by adding a check for IN_BLE
 device for not to disconnect.

Change-Id: I59b3ffb0fe2eb2be3cab698c595e6e72d2061011

resource_manager/src/ResourceManager.cpp
@@ -7963,8 +7963,9 @@ int32_t ResourceManager::a2dpResume(pal_device_id_t dev_id)
                 (*sIter)->getAssociatedDevices(devices);
                 if (devices.size() > 0) {
                     for (auto device: devices) {
-                        if (device->getSndDeviceId() > PAL_DEVICE_OUT_MIN &&
-                            device->getSndDeviceId() < PAL_DEVICE_OUT_MAX) {
+                        if ((device->getSndDeviceId() > PAL_DEVICE_OUT_MIN &&
+                            device->getSndDeviceId() < PAL_DEVICE_OUT_MAX) &&
+                            ((*sIter)->suspendedDevIds.size() == 1 /* non combo */)) {
                             streamDevDisconnect.push_back({ (*sIter), device->getSndDeviceId() });
                         }
                     }
@@ -11345,7 +11346,9 @@ bool ResourceManager::doDevAttrDiffer(struct pal_device *inDevAttr,
         ((inDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_BLE) &&
         (curDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_BLE)) ||
         ((inDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_BLE_BROADCAST) &&
-        (curDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_BLE_BROADCAST))) {
+        (curDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_BLE_BROADCAST)) ||
+        ((inDevAttr->id == PAL_DEVICE_IN_BLUETOOTH_BLE) &&
+        (curDevAttr->id == PAL_DEVICE_IN_BLUETOOTH_BLE))) {
         pal_param_bta2dp_t *param_bt_a2dp = nullptr;
 
         if (isDeviceAvailable(inDevAttr->id)) {

xiaomi-sm6225/android_vendor_qcom_opensource_arpal-lx

resource_manager/src/ResourceManager.cpp
@@ -11936,7 +11936,9 @@ bool ResourceManager::doDevAttrDiffer(struct pal_device *inDevAttr,
     if (((inDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_A2DP) &&
         (curDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_A2DP)) ||
         ((inDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_BLE) &&
-        (curDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_BLE))) {
+        (curDevAttr->id == PAL_DEVICE_OUT_BLUETOOTH_BLE)) ||
+        ((inDevAttr->id == PAL_DEVICE_IN_BLUETOOTH_BLE) &&
+        (curDevAttr->id == PAL_DEVICE_IN_BLUETOOTH_BLE))) {
         pal_param_bta2dp_t *param_bt_a2dp = nullptr;
 
         if (isDeviceAvailable(inDevAttr->id)) {